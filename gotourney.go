e918ed8a3cbbadb52e72985f1aed84a208eaf3da
Ed Copony <ecopony@gmail.com>
2016-03-30 21:16:32 -0700

    Add template

--- gotourney.go
+++ gotourney.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "welcome to gotourney")
+	p := &PageData{Title: "gotourney", Username: "ecopony"}
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, p)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
+}
+
+type PageData struct {
+	Title    string
+	Username string
 }
