91fc221b4101852f359086e6d19329e9c9f610cd
Ed Copony <ecopony@gmail.com>
2016-04-02 15:01:27 -0700

    Make template compilation only happen once

--- gotourney.go
+++ gotourney.go
@@ -3,17 +3,29 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"sync"
+	"path/filepath"
+	"log"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	p := &PageData{Title: "gotourney", Username: "ecopony"}
-	t, _ := template.ParseFiles("index.html")
-	t.Execute(w, p)
+func main() {
+	http.Handle("/", &templateHandler{filename: "index.html"})
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("Listen and serve: ", err)
+	}
 }
 
-func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	tmpl    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.tmpl = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.tmpl.Execute(w, &PageData{Title: "gotourney", Username: "ecopony"})
 }
 
 type PageData struct {
